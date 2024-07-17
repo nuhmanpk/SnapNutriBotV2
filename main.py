636a25ba820446120fe826113e67ea08a216350b
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-07-17 10:58:30 +0000

    feat: session string added

--- main.py
+++ main.py
@@ -10,13 +10,20 @@
 
 from decouple import config
 from hydrogram import Client
-# from pyromod import Client
 
 BOT_TOKEN = config('BOT_TOKEN')
 API_ID = config('API_ID', cast=int)
 API_HASH = config('API_HASH')
 SESSION_STRING = config('SESSION_STRING')
 
+# async def main():
+#     async with Client(":memory:", api_id=int(API_ID), api_hash=API_HASH) as app:
+#         # Generate the session string
+#         session_string = await app.export_session_string()
+#         print("Your session string is:", session_string)
+
+# import asyncio
+# asyncio.run(main())
 
 Bot = Client(
     "Crypto Bot",
