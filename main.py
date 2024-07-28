3cd4fdd6f63cc927e00f81ab3fe80b5ab5db2722
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-07-28 19:12:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,16 +16,6 @@ from bot.scheduler import start_scheduler
 BOT_TOKEN = config('BOT_TOKEN')
 API_ID = config('API_ID', cast=int)
 API_HASH = config('API_HASH')
-SESSION_STRING = config('SESSION_STRING')
-
-# async def main():
-#     async with Client(":memory:", api_id=int(API_ID), api_hash=API_HASH) as app:
-#         # Generate the session string
-#         session_string = await app.export_session_string()
-#         print("Your session string is:", session_string)
-
-# import asyncio
-# asyncio.run(main())
 
 Bot = Client(
     "SnapNutriV2 Bot",
@@ -33,8 +23,6 @@ Bot = Client(
     api_id=API_ID,
     api_hash=API_HASH,
     plugins=dict(root="bot"),
-    in_memory=True,
-    session_string=SESSION_STRING
 )
 
 if __name__ == "__main__":
