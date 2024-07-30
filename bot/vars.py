2f2d7cb97c5d9769fd83c628db640c7c1177be73
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-07-30 09:17:48 +0530

    Update vars.py

--- bot/vars.py
+++ bot/vars.py
@@ -5,5 +5,5 @@ ADMINS = config('ADMINS', cast=lambda v: set(int(x) for x in v.split()))
 DATABASE_URL = config('DATABASE_URL')
 DATABASE_NAME = config('DATABASE_NAME')
 GOOGLE_API_KEY = config("GOOGLE_API_KEY")
-GEMINI_VISION = "gemini-pro-vision"
+GEMINI_VISION = "gemini-1.5-pro"
 GEMINI_FLASH_1_5 = "gemini-1.5-flash"
