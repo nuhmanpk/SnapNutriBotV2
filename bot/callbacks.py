d581995d504215c992efa3853da64ce196121c35
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-08-03 13:46:16 +0530

    refactor: correct callback error

--- bot/callbacks.py
+++ bot/callbacks.py
@@ -4,7 +4,17 @@ from hydrogram import Client
 from hydrogram.types import Message
 from hydrogram import filters
 from hydrogram.types import CallbackQuery
-from .commands import start, help, about
+from .constants import (
+    START_TEXT,
+    HELP_TEXT,
+    ABOUT_TEXT
+)
+from .buttons import (
+    HELP_BUTTONS, 
+    START_BUTTONS, 
+    ABOUT_BUTTONS,
+    CLOSE_BUTTON
+)
 
 
 @Client.on_callback_query()
@@ -12,12 +22,33 @@ async def cb_data(_, message):
     await add_user(message.from_user.id)
     if message.data == "home":
         await message.message.delete()
-        await start(_, message, cb=True)
+        message = message.message
+        await message.reply_text(
+        text=START_TEXT,
+        reply_markup=START_BUTTONS,
+        disable_web_page_preview=True,
+        quote=True,
+        )
+        # await start(_, message, cb=True)
     elif message.data == "help":
         await message.message.delete()
-        await help(_, message, cb=True)
+        message = message.message
+        await message.reply_text(
+        text=HELP_TEXT,
+        reply_markup=HELP_BUTTONS,
+        disable_web_page_preview=True,
+        quote=True,
+        )
+        # await help(_, message, cb=True)
     elif message.data == "about":
         await message.message.delete()
+        message = message.message
+        await message.reply_text(
+        text=ABOUT_TEXT,
+        reply_markup=ABOUT_BUTTONS,
+        disable_web_page_preview=True,
+        quote=True,
+        )
         await about(_, message, cb=True)
     elif message.data == "close":
         await message.message.delete()
--- bot/commands.py
+++ bot/commands.py
@@ -23,16 +23,7 @@ from .stickers import LOADING_STICKERS
 
 
 @Client.on_message(filters.private & filters.command(["start"]))
-async def start(bot, message, cb=False):
-    if cb:
-        message = message.message
-        await message.reply_text(
-        text=START_TEXT,
-        reply_markup=START_BUTTONS,
-        disable_web_page_preview=True,
-        quote=True,
-        )
-        return
+async def start(bot, message):
     await add_user(message.from_user.id)
     await message.reply_text(
         text=START_TEXT,
@@ -43,16 +34,7 @@ async def start(bot, message, cb=False):
 
 
 @Client.on_message(filters.private & filters.command(["help"]))
-async def help(bot, message, cb=False):
-    if cb:
-        message = message.message
-        await message.reply_text(
-        text=HELP_TEXT,
-        reply_markup=HELP_BUTTONS,
-        disable_web_page_preview=True,
-        quote=True,
-        )
-        return
+async def help(bot, message:
     await add_user(message.from_user.id)
     await message.reply_text(
         text=HELP_TEXT,
@@ -63,16 +45,7 @@ async def help(bot, message, cb=False):
 
 
 @Client.on_message(filters.private & filters.command(["about"]))
-async def about(bot, message, cb=False):
-    if cb:
-        message = message.message
-        await message.reply_text(
-        text=ABOUT_TEXT,
-        reply_markup=ABOUT_BUTTONS,
-        disable_web_page_preview=True,
-        quote=True,
-        )
-        return
+async def about(bot, message):
     await add_user(message.from_user.id)
     await message.reply_text(
         text=ABOUT_TEXT,
