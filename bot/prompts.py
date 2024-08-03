0f225fc648e54c210dc0ef88c2a549d7a088b357
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-08-03 14:06:09 +0530

    fix: improve prompt for better result

--- bot/prompts.py
+++ bot/prompts.py
@@ -20,13 +20,14 @@ If the image is recognizable take a wild guess , or else the response should be:
   "status": true,
   information: somedetails about the image
 }
+
+if there is no image of food items or any kind of meals or items that are inedible , the response should be :
+{
+  "status": false
+}
+
 """
 
 DAILY_TIPS_PROMPT = """Generate a brief, motivational health tip (max 3 lines) with a fun fact or medical benefit. 
 Also include a advantage of including something specific on daily intake to make the diet more healthier, Include emojis for engagement"""
 
-
-# If the image is recognizable take a wild guess , or else the response should be:
-# {
-#   "status": false
-# }
